f43d130f34fc1c6c1ac3ea7268c3d24df2bfc361
Alan Johnston <alan@infinpc.com>
2016-06-09 06:47:57 +0700

    global doting, dev and test environments

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const NODE_ENV = process.env.NODE_ENV;
+const dotenv = require('dotenv');
+
 const webpack = require('webpack');
 const fs      = require('fs');
 const path    = require('path'),
@@ -6,13 +9,13 @@ const path    = require('path'),
 
 const getConfig = require('hjs-webpack');
 
+const isDev  = NODE_ENV === 'development';
+const isTest = NODE_ENV === 'test';
+
 const root    = resolve(__dirname);
 const src     = join(root, 'src');
 const modules = join(root, 'node_modules');
 const dest    = join(root, 'dist');
-
-const NODE_ENV = process.env.NODE_ENV;
-const isDev = NODE_ENV === 'development';
 
 
 var config = getConfig({
@@ -21,6 +24,32 @@ var config = getConfig({
   out: dest,
   clearBeforeBuild: true
 })
+
+
+// ENV variables
+const dotEnvVars = dotenv.config();
+const environmentEnv = dotenv.config({
+  path: join(root, 'config', `${NODE_ENV}.config.js`),
+  silent: true,
+});
+const envVariables =
+    Object.assign({}, dotEnvVars, environmentEnv);
+
+const defines =
+  Object.keys(envVariables)
+  .reduce((memo, key) => {
+    const val = JSON.stringify(envVariables[key]);
+    memo[`__${key.toUpperCase()}__`] = val;
+    return memo;
+  }, {
+    __NODE_ENV__: JSON.stringify(NODE_ENV)
+  });
+
+config.plugins = [
+  new webpack.DefinePlugin(defines)
+].concat(config.plugins);
+// END ENV variables
+
 
 
 // CSS modules

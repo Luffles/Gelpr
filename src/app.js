28eaa6626500e524933d7b89f57bd01b0818c5be
Alan Johnston <alan@infinpc.com>
2016-06-10 10:29:04 +0700

    Font-awesome module integration and test

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import 'font-awesome/css/font-awesome.css'
 import './app.css'
 import styles from './styles.module.css'
 
@@ -8,7 +9,9 @@ const App = React.createClass({
   render: function() {
     return (
       <div className={styles.wrapper}>
-        <h1>Environment: {__NODE_ENV__}</h1>
+        <h1>
+          <i className="fa fa-star"></i>
+          Environment: {__NODE_ENV__}</h1>
       </div>
     )
   }

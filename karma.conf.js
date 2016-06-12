6cd9c82b161c8b33bd01d979476dd2579588f7b3
Alan Johnston <alan@infinpc.com>
2016-06-12 11:53:59 +0700

    Karma frameworks set to mocha and chai, respective plugins added to cone

--- karma.conf.js
+++ karma.conf.js
@@ -10,8 +10,11 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
+    frameworks: ['mocha', 'chai'],
+    plugins: [
+      'karma-mocha',
+      'karma-chai'
+    ],
 
     // list of files / patterns to load in the browser
     files: [

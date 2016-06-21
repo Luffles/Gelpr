e7331160ab0d4ff04a25cdd4d5660998623f0591
Alan Johnston <alan@infinpc.com>
2016-06-21 10:31:59 +0700

    Loading google map

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Map, {GoogleApiWrapper} from 'google-maps-react'
+
 
 export class Container extends React.Component {
   render() {
     return (
       <div>
         Hello from the container
+        <Map
+          google={this.props.google} />
       </div>
     )
   }

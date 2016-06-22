04d9f123d889cc5a2a64cd0b0f01394e8b51f041
Alan Johnston <alan@infinpc.com>
2016-06-22 10:17:45 +0700

    container with map api, also completed but excluded: export in .env gapi key

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,17 +1,55 @@
 import React from 'react'
 import Map, {GoogleApiWrapper} from 'google-maps-react'
-
+import {searchNearby} from 'utils/googleApiHelpers'
 
 export class Container extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      places: [],
+      pagination: null
+    }
+  }
+
+  onReady(mapProps, map) {
+    searchNearby(
+      this.props.google,
+      map,
+      {
+        location: map.center,
+        radius: '500',
+        types: ['cafe']
+      }
+    ).then((results, pagination) => {
+      this.setState({
+        places: results,
+        pagination
+      })
+    }).catch((status) => {
+      console.log('error fetching nearby', status)
+    })
+  }
+  
   render() {
     return (
       <div>
         Hello from the container
         <Map
-          google={this.props.google} />
+          google={this.props.google}
+          onReady={this.onReady.bind(this)}
+          visible={false}>
+
+          {this.state.places.map(place => {
+            return (<div key={place.id}>{place.name}</div>)
+          })}
+
+        </Map>
       </div>
     )
   }
 }
 
-export default Container
+export default GoogleApiWrapper({
+  apiKey: __GAPI_KEY__
+})(Container)

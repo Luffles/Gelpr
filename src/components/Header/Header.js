cf9020e78f3dee2bbc5ccac59bbca9ea30288306
Alan Johnston <alan@infinpc.com>
2016-06-29 11:38:59 +0700

    Header init test

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router'
+import styles from './styles.module.css';
+
 
 export class Header extends React.Component {
   render() {
     return (
-      <div>
+      <div className={styles.topbar}>
         <Link to="/"><h1>Yelp</h1></Link>
         <section>
           Fullstack.io
